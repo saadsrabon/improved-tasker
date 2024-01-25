403d33f814b45fa6553b7293a7dc4622a037687c
Saad Srabon <saadsrabon2@gmail.com>
2024-01-25 12:59:18 +0600

    2024-01-25 12:59:18 - hero component breakdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
-import logo from './assets/lws-logo-en.svg'
+
 import frame from './assets/frame.png'
+import Navbar from './components/Navbar'
 function App() {
   
   return (
     <>
       {/* <!-- Navbar Starts --> */}
-	<nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
-		<div className="container mx-auto flex items-center justify-between gap-x-6">
-			{/* <!-- Logo --> */}
-			<a href="/">
-				<img className="h-[45px]" src={logo} alt="Lws" />
-			</a>
-			{/* <!-- Logo Ends --> */}
-		</div>
-	</nav>
+	      <Navbar />
 	{/* <!-- Navbar Ends --> */}
 
 	{/* <!-- Begin hero --> */}
-	<section className="pb-[114px] pt-20 md:mt-[100px]">
-		<div className="container lg:px-20">
-			<div className="grid items-center gap-6 md:grid-cols-2">
-				<div className="flex justify-center md:order-2">
-					<img className="max-md:w-full" src={frame} width="326" height="290" alt="frame" />
-				</div>
-				<div>
-					<h1 className="mb-1.5 text-[56px] font-bold leading-none text-[#F5BF42] lg:text-[73px]">
-						Tasker
-					</h1>
-					<p className="text-lg my-2 opacity-60">
-						Effortlessly Organize, Prioritize, and Conquer Tasks with Tasker - Your Personal Productivity Ally for
-						Seamless Goal Achievement and Stress-Free Task Management.
-					</p>
-				</div>
-			</div>
-		</div>
-	</section>
+
 	{/* <!-- End Hero --> */}
 
 	{/* <!-- Begin Table --> */}
